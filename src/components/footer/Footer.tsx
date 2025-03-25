2065d0b3c4190a0cc8db7527a2090590a8034668
Akshay Ranjan <akshay.ranjan@gmail.com>
2025-03-25 09:51:12 +0530

    Rework different links.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,56 @@
-import { Group, Image, Stack, useMantineColorScheme } from "@mantine/core";
+import {
+  ActionIcon,
+  Group,
+  Image,
+  Stack,
+  Text,
+  useMantineColorScheme,
+} from "@mantine/core";
+import {
+  IconBrandInstagram,
+  IconBrandLinkedin,
+  IconBrandX,
+} from "@tabler/icons-react";
 
-interface FooterProps {
-  footerLinks?: React.ReactNode;
-  children?: React.ReactNode;
-}
-function Footer(props: FooterProps) {
+const footerLinks = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "People",
+    href: "/people",
+  },
+  {
+    label: "Technologies",
+    href: "/technologies",
+  },
+  {
+    label: "About Us",
+    href: "/about",
+  },
+  {
+    label: "Contact Us",
+    href: "/contact",
+  },
+];
+
+const footerSocialLinks = [
+  {
+    icon: <IconBrandLinkedin size={26} stroke={1.0} />,
+    href: "https://www.linkedin.com/company/arc-technologies/",
+  },
+  {
+    icon: <IconBrandX size={26} stroke={1.0} />,
+    href: "https://twitter.com/arc_technologies",
+  },
+  {
+    icon: <IconBrandInstagram size={26} stroke={1.0} />,
+    href: "https://www.instagram.com/arc_technologies/",
+  },
+];
+
+function Footer() {
   const { colorScheme } = useMantineColorScheme();
 
   return (
@@ -27,11 +73,33 @@ function Footer(props: FooterProps) {
             />
           )}
         </Group>
-        <Group>{props.footerLinks}</Group>
+        <Group>
+          <Stack w={"100%"} align={"flex-end"}>
+            <Group gap={"sm"}>
+              {footerLinks.map((item, index) => (
+                <Text key={index} size={"sm"}>
+                  {item.label}
+                </Text>
+              ))}
+            </Group>
+            <Group gap={"xs"}>
+              {footerSocialLinks.map((item, index) => (
+                <ActionIcon key={index} variant="subtle">
+                  {item.icon}
+                </ActionIcon>
+              ))}
+            </Group>
+            <Group gap={"xs"}>
+              <Text size={"xs"} c={"dimmed"}>
+                © {new Date().getFullYear()} ARC Technologies. All rights
+                reserved.
+              </Text>
+            </Group>
+          </Stack>
+        </Group>
       </Group>
     </Stack>
   );
 }
 
 export { Footer };
-export type { FooterProps };
